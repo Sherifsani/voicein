65a608f7acaa8a2870574aee00675eb0045833f2
Sherif Sani <sanisherif11@gmail.com>
2025-07-03 14:24:32 +0100

    Refactor Home component to improve structure and readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import React from "react";
+import HeroPage from "./components/HeroPage";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Pricing from "./components/Pricing";
+import Faqs from "./components/Faqs";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
-
+      <HeroPage />
+      <Features />
+      <HowItWorks />
+      <Pricing />
+      <Faqs />
+      <Footer />
     </main>
   );
 }
